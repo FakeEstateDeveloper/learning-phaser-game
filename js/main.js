233f6c2bb78b24e1d40cef86a4596d809ee998da
FakeEstateDeveloper <hormonalpokeman@gmail.com>
2026-03-02 19:10:10 +0800

    Started modular file structure

---
+++ js/main.js
@@ -0,0 +1,176 @@
+// Command to run the game
+// python -m http.server 8000
+
+// Website to run the game
+// http://localhost:8000/
+
+// Github link
+// https://FakeEstateDeveloper.github.io/learning-phaser-game/
+
+export function main(Phaser, createStates) {
+    let player, ground, bg, states;
+    let currentState;
+    let currentSpeed = 1;
+    
+    const config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: "#000000",
+        scene: { preload, create, update },
+        physics: {
+            default: "arcade",
+            arcade: {
+                gravity: { y: 300 },
+                debug: true
+            }
+        }
+    };
+
+const game = new Phaser.Game(config);
+
+// Preload
+function preload() {
+    // Preload Background
+    this.load.image("background", "assets/background.png");
+
+    // Preload Ground
+    this.load.image("ground", "https://i.imgur.com/Zmp7rQG.png");
+
+    // #region Preload Hero
+    // Hero spritesheets
+        this.load.spritesheet("hero_idle",                  "assets/hero/120x80pngsheets/idle.png",                         { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_run",                   "assets/hero/120x80pngsheets/run.png",                          { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_crouch",                "assets/hero/120x80pngsheets/crouch.png",                       { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_crouch_walk",           "assets/hero/120x80pngsheets/crouchwalk.png",                   { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_attack",                "assets/hero/120x80pngsheets/attack.png",                       { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_jump",                  "assets/hero/120x80pngsheets/jump.png",                         { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_fall",                  "assets/hero/120x80pngsheets/fall.png",                         { frameWidth: 120, frameHeight: 80 });
+        this.load.spritesheet("hero_jumpfalltransition",    "assets/hero/120x80pngsheets/jumpfalltransition.png",           { frameWidth: 120, frameHeight: 80 });
+    //#endregion
+}
+
+// Create
+function create() {
+    // Create Background
+    bg = this.add.image(0, 0, "background").setOrigin(0, 0);
+
+    // #region Create Hero
+    // Idle
+    this.anims.create({
+        key: "idle",
+        frames: this.anims.generateFrameNumbers("hero_idle", { start: 0, end: 9 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    // Run
+    this.anims.create({
+        key: "run",
+        frames: this.anims.generateFrameNumbers("hero_run", { start: 0, end: 9 }),
+        frameRate: 12,
+        repeat: -1
+    });
+    // Crouch
+    this.anims.create({
+        key: "crouch",
+        frames: this.anims.generateFrameNumbers("hero_crouch", { start: 0, end: 0 })
+    });
+    // CrouchWalk
+    this.anims.create({
+        key: "crouch_walk",
+        frames: this.anims.generateFrameNumbers("hero_crouch_walk", { start: 0, end: 7 }),
+        frameRate: 9,
+        repeat: -1
+    });
+    // Attack
+    this.anims.create({
+        key: "attack",
+        frames: this.anims.generateFrameNumbers("hero_attack", { start: 0, end: 3 }),
+        frameRate: 20,
+        repeat: 0
+    });
+    // Jump
+    this.anims.create({
+        key: "jump",
+        frames: this.anims.generateFrameNumbers("hero_jump", { start: 0, end: 2}),
+        frameRate: 20,
+        repeat: 0
+    });
+    // Fall
+    this.anims.create({
+        key: "fall",
+        frames: this.anims.generateFrameNumbers("hero_fall", { start: 0, end: 2}),
+        frameRate: 20,
+        repeat: 0
+    });
+    // Jumpfalltransition
+    this.anims.create({
+        key: "jumpfalltransition",
+        frames: this.anims.generateFrameNumbers("hero_jumpfalltransition", { start: 0, end: 1}),
+        frameRate: 30,
+        repeat: 0
+    });
+    // #endregion
+
+    // Create Movement
+    this.keys = this.input.keyboard.addKeys({
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D,
+        crouch: Phaser.Input.Keyboard.KeyCodes.C,
+        jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
+        dodgeroll: Phaser.Input.Keyboard.KeyCodes.Q
+    });
+
+    // #region Create Setup
+    // Spawn point
+    const spawnX = bg.width / 2;
+    const spawnY = bg.height - 400;
+
+    // Create Ground
+    ground = this.physics.add.staticGroup();
+    ground.create(spawnX, spawnY + 300, "ground").setScale(2).refreshBody();
+
+    // Create more platforms
+    ground.create(spawnX + 10, spawnY + 210, "ground").setScale(0.1).refreshBody();
+    ground.create(spawnX + 100, spawnY + 180, "ground").setScale(0.1).refreshBody();
+
+    // Spawn player
+    player = this.physics.add.sprite(spawnX, spawnY, "hero_idle");
+    player.body.setSize(25.5, 40);
+    player.body.setOffset(47.5, 40);
+
+    // Add collision between player and ground
+    this.physics.add.collider(player, ground);
+
+    // Make camera unable to show black void
+    this.cameras.main.setBounds(0, 0, bg.width, bg.height);
+
+    // Make camera follow the player
+    this.cameras.main.startFollow(player, true, 0.1, 0.1);
+
+    // Disables right-click mouse menu
+    this.input.mouse.disableContextMenu();
+    
+    // Flags
+    this.canAttack = true;
+
+    // Enter State
+    states = createStates(player, currentSpeed);
+    currentState = "idle";
+    states[currentState].onEnter();
+    // #endregion
+}
+
+// Update
+function update() {
+    // Enter Next State
+    const nextState = states[currentState].onUpdate(this);
+    if (nextState !== currentState) {
+        states[currentState].onExit(this);
+        states[nextState].onEnter(this);
+        currentState = nextState;
+        console.log("Switched to state:", currentState);
+    }
+}
+
+}
---
+++ js/statemachine.js
@@ -0,0 +1,188 @@
+// State Machine
+export function createStates(player, currentSpeed) {
+    return {
+        idle: {
+            onEnter() {
+                player.play("idle", true);
+            },
+            onUpdate(scene) {
+                // Attack
+                const mouse = scene.input.activePointer;
+                if (mouse.leftButtonDown() && scene.canAttack && mouse.button === 0) {
+                    scene.canAttack = false;    // "Lock" the attack
+                    return "attack";
+                }
+                // Reset Attack
+                if (!mouse.leftButtonDown()) {
+                    scene.canAttack = true;
+                }
+                // Jump
+                if (scene.keys.jump.isDown && player.body.touching.down) return "jump";
+                // Crouch
+                if (scene.keys.crouch.isDown) return "crouch";
+                // Run
+                if (scene.keys.left.isDown || scene.keys.right.isDown) return "run";
+                // Idle
+                return "idle";
+            },
+            onExit() {}
+        },
+        run: {
+            onEnter() {
+                player.play("run", true);
+            },
+            onUpdate(scene) {
+                // Attack
+                const mouse = scene.input.activePointer;
+                if (mouse.leftButtonDown() && scene.canAttack && mouse.button === 0) {
+                    scene.canAttack = false;    // "Lock" the attack
+                    return "attack";
+                }
+                // Reset Attack
+                if (!mouse.leftButtonDown()) {
+                    scene.canAttack = true;
+                }
+                // Jump
+                if (scene.keys.jump.isDown && player.body.touching.down) return "jump";
+                // Fall
+                if (player.body.velocity.y > 0) return "fall";
+                // Crouch Walk
+                if (scene.keys.crouch.isDown) return "crouch_walk";
+                // Idle
+                if (!(scene.keys.left.isDown || scene.keys.right.isDown)) return "idle";
+
+                // Movement Logic
+                if (scene.keys.left.isDown) {
+                    player.x -= currentSpeed;
+                    player.flipX = true;
+                }
+                else if (scene.keys.right.isDown) {
+                    player.x += currentSpeed;
+                    player.flipX = false;
+                }
+                return "run";
+            },
+            onExit() {
+                player.setVelocityX(0);
+            }
+        },
+        crouch: {
+            onEnter() {
+                player.play("crouch", true);
+            },
+            onUpdate(scene) {
+                // Idle
+                if (!scene.keys.crouch.isDown) return "idle";
+                // Crouch Walk
+                if (scene.keys.left.isDown || scene.keys.right.isDown) return "crouch_walk";
+                // Crouch
+                return "crouch";
+            },
+            onExit() {}
+        },
+        crouch_walk: {
+            onEnter() {
+                player.play("crouch_walk", true);
+            },
+            onUpdate(scene) {
+                // Run
+                if (!scene.keys.crouch.isDown) return "run";
+                // Crouch
+                if (!scene.keys.left.isDown && !scene.keys.right.isDown) return "crouch";
+
+                // Slowed Movement
+                const crouchSpeed = currentSpeed / 2;
+                // Movement Logic
+                if (scene.keys.left.isDown) {
+                    player.x -= crouchSpeed;
+                    player.flipX = true;
+                }
+                else if (scene.keys.right.isDown) {
+                    player.x += crouchSpeed;
+                    player.flipX = false;
+                }
+                // Crouch Walk
+                return "crouch_walk";
+            },
+            onExit() {
+                player.setVelocityX(0);
+            }
+        },
+        attack: {
+            isFinished: false,
+            onEnter() {
+                this.isFinished = false;
+                player.play("attack", true)
+
+                // Once the animation finishes, set our flag to true
+                player.once("animationcomplete-attack", () => {
+                    this.isFinished = true;
+                });
+            },
+            onUpdate() {
+                // Fall
+                if (!player.body.touching.down) return "fall";
+                // Stay in attack state until the animation is done
+                if (this.isFinished) {
+                    return "idle";
+                }
+                return "attack";
+            },
+            onExit() {
+                this.isFinished = false;
+            }
+        },
+        jump: {
+            onEnter() {
+                player.setVelocityY(-150);
+                player.play("jump", true);
+            },
+            onUpdate(scene) {
+                // Movement Logic
+                if (scene.keys.left.isDown) {
+                    player.x -= currentSpeed;
+                    player.flipX = true;
+                }
+                else if (scene.keys.right.isDown) {
+                    player.x += currentSpeed;
+                    player.flipX = false;
+                }
+                // Fall
+                if (player.body.velocity.y > 0) {
+                    return "fall";
+                }
+                // Still going up
+                return "jump";
+            },
+            onExit() {
+                player.setVelocityX(0);
+                player.play("jumpfalltransition", true);
+            }
+        },
+        fall: {
+            onEnter() {
+                player.play("fall", true);
+            },
+            onUpdate(scene) {
+                // Movement Logic
+                if (scene.keys.left.isDown) {
+                    player.x -= currentSpeed;
+                    player.flipX = true;
+                }
+                else if (scene.keys.right.isDown) {
+                    player.x += currentSpeed;
+                    player.flipX = false;
+                }
+                // Landed
+                if (player.body.touching.down) {
+                    if (scene.keys.left.isDown || scene.keys.right.isDown) return "run";
+                    return "idle";
+                }
+                return "fall"; // still falling
+            },
+            onExit() {
+                player.setVelocityX(0);
+            }
+        }
+    };
+}
